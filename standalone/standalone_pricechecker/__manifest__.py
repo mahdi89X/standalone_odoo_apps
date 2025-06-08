5df4afba44d40a63548d5e189e6a7e711a593dc3
mahdi89X <medizz@hotmail.com>
2025-06-08 19:13:21 +0300

    Add new files to 8.0 branch

---
+++ standalone/standalone_pricechecker/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Price Checker Sandalone For Screens test",
+    'version': '18.0.0.0',
+    'license': 'LGPL-3',
+    "summary": "Standalone barcode price checker",
+    "description": "A simple module to check product price, quantity, and image by barcode.",
+    "author": "Mahdi Farhat",
+    "category": "Tools",
+    "depends": ["base"],
+    "data": [
+        "views/price_checker_template.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False
+}
